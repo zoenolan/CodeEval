8209f06456fa4ecb902076c1854236cd2d3d20d6
Zoe Nolan <github@zoenolan.org>
2016-01-09 06:20:54 +0000

    Add odd numbers challenge in C

---
+++ Easy Level/Odd Numbers/Odd Numbers.c
@@ -0,0 +1,13 @@
+// Odd Numbers Code Eval Challenges
+
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) 
+{
+	for (uint8_t counter = 1; counter < 100; counter +=2)
+	{
+		printf("%d\n", counter);
+	}
+}
+
